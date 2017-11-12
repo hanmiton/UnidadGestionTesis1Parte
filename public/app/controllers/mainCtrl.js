71d93760e1931459e89360e659e707f6116370c0
hanmiton <hanmilton_12@outlook.com>
2017-11-12 01:14:57 -0500

    añadiendo ingeniero des controlador

--- public/app/controllers/mainCtrl.js
+++ public/app/controllers/mainCtrl.js
@@ -4,6 +4,24 @@ angular.module('mainController',['authServices', 'userServices'])
 	//console.log('hanmilton')
 	var app = this;
  	
+
+	//nuevo codigo admin ingeniero
+
+	this.ingeniero = {
+      id: "001",
+      name: "Nombre001",
+      ingenieria: "Ingenieria001",
+      tipo: [ "Tipo1", "Tipo2" ],
+      edad: "40",
+      facultad: "facultad001",
+      habilidades: [ "habilidad1", "habilidad2"]
+    };
+
+
+
+
+
+
  	app.loadme = false;
 
  	app.checkSession = function() {
