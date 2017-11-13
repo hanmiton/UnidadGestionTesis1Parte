14807bdc49d7f19ead946f616cf75ac98fa19d36
hanmiton <hanmilton_12@outlook.com>
2017-11-13 00:06:46 -0500

    añadiendo ingeniero json completo

--- public/app/controllers/mainCtrl.js
+++ public/app/controllers/mainCtrl.js
@@ -6,7 +6,6 @@ angular.module('mainController',['authServices', 'userServices'])
  	
 
 	//nuevo codigo admin ingeniero
-
 	this.ingeniero = {
       id: "001",
       name: "Nombre001",
@@ -14,7 +13,17 @@ angular.module('mainController',['authServices', 'userServices'])
       tipo: [ "Tipo1", "Tipo2" ],
       edad: "40",
       facultad: "facultad001",
-      habilidades: [ "habilidad1", "habilidad2"]
+      habilidades: [ "habilidad1", "habilidad2"],
+       stats: {
+        st1: 45,
+        st2: 49,
+        st3: 49,
+        "sp.st4": 65,
+        "sp.st5": 65,
+        st6: 45,
+        total: 318
+      },
+      evolution: [ "Ingeniero", "Masterado", "Doctorado" ]
     };
 
 
